cf0d6c5c80a72dcd5beffcd244c355961ec140ab
AlicanHasirci <alican@peakgames.net>
2015-01-28 12:12:17 +0200

    Added DummyEventService for Desktop keyboard

---
+++ core/src/main/java/net/peakgames/libgdx/stagebuilder/core/keyboard/DummyKeyboardEventService.java
@@ -0,0 +1,21 @@
+package net.peakgames.libgdx.stagebuilder.core.keyboard;
+
+/**
+ * Created by alican on 28/01/15.
+ */
+public class DummyKeyboardEventService implements SoftKeyboardEventInterface {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void setSoftKeyboardEventListener(SoftKeyboardEventListener eventListener) {
+
+    }
+
+    @Override
+    public void focusChanged() {
+
+    }
+}
