d1ca2c9635de88706cff94d07f20a4af5a8f3a61
muratcanbulut <bulut.muratcan@gmail.com>
2015-03-30 16:01:33 +0300

    Fixed desktop demo crash.

--- core/src/main/java/net/peakgames/libgdx/stagebuilder/core/keyboard/DummyKeyboardEventService.java
+++ core/src/main/java/net/peakgames/libgdx/stagebuilder/core/keyboard/DummyKeyboardEventService.java
@@ -1,8 +1,5 @@
 package net.peakgames.libgdx.stagebuilder.core.keyboard;
 
-/**
- * Created by alican on 28/01/15.
- */
 public class DummyKeyboardEventService implements SoftKeyboardEventInterface {
     @Override
     public void initialize() {
